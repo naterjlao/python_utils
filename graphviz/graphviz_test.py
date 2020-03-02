4bdb1b73cff2305d758e8d90d1c4f7e6e762e4f5
Nate Lao <lao.nathan@yahoo.com>
2020-03-01 19:38:39 -0500

    fixed comma

--- graphviz/graphviz_test.py
+++ graphviz/graphviz_test.py
@@ -3,8 +3,8 @@ import graphviz
 dot = graphviz.Digraph(comment='The round Table')
 
 dot.node('A', 'King Arthur')
-dot.node('B' 'Sir Bedevere the Wise')
-dot.node('L' 'Sir Lancelot the Brave')
+dot.node('B','Sir Bedevere the Wise')
+dot.node('L','Sir Lancelot the Brave')
 
 dot.edges(['AB','AL'])
 dot.edge('B','L',constraint='false')
